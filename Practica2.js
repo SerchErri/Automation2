02ab64572b82dc1ddc474d36417adf1aa36e4e3a
SerchErri <106555563+SerchErri@users.noreply.github.com>
2025-07-11 00:49:00 -0300

    Correcciones Practica2

--- Practica2.js
+++ Practica2.js
@@ -10,44 +10,67 @@ var reservacionActual = {
 function validarHuesped(nombreHuesped) {
     if (!nombreHuesped) {
         console.log("Debe ingresar un nombre válido");
+        console.log("----------------------");
     }
     switch (nombreHuesped) {
         case "Maria":
             console.log("El cliente es incorrecto");
+            console.log("----------------------");
             break;
         case "Sergio":
             console.log("El cliente es correcto");
+            console.log("----------------------");
             break;
         default:
             console.log("Cliente no encontrado");
+            console.log("----------------------");
     }
 }
 function validarCantidadNoches(reservacionActual) {
     switch (true) {
         case (reservacionActual.cantidadNoches >= 3):
             console.log("La Reserva se efectua con éxito");
+            console.log("----------------------");
             break;
-        case (reservacionActual.cantidadNoches < 3):
+        case (reservacionActual.cantidadNoches > 1 && reservacionActual.cantidadNoches < 3):
             console.log("La cantidad minima de noches son 3");
+            console.log("----------------------");
             break;
         default:
             console.log("Debe colocar una cantidad de noches válida");
+            console.log("----------------------");
     }
 }
+function validarPagoRealizado(reservacionActual) {
+    switch (reservacionActual.pagoRealizado) {
+        case true:
+            console.log("El pago se ha realizado correctamente");
+            console.log("----------------------");
+            break;
+        case false:
+            console.log("El pago no se ha realizado");
+            console.log("----------------------");
+            break;
+        default:
+            console.log("Debe colocar un estado de pago válido");
+            console.log("----------------------");
+    }
+}
+// ----------- Llamamadas a las funciones de validación -----------
 validarHuesped("Sergio");
 validarHuesped("Maria");
-
-// Ejemplo 1: mayor a 3
 var reserva1 = {
     nombreHuesped: "Sergio",
     cantidadNoches: 5,
     pagoRealizado: true
 };
-validarCantidadNoches(reserva1); 
+validarCantidadNoches(reserva1);
 // Ejemplo 2: menor a 3
 var reserva2 = {
     nombreHuesped: "Lucía",
-    cantidadNoches: 2,
+    cantidadNoches: 0,
     pagoRealizado: false
 };
 validarCantidadNoches(reserva2);
+validarPagoRealizado(reserva1);
+validarPagoRealizado(reserva2);
--- Practica2.ts
+++ Practica2.ts
@@ -18,20 +18,24 @@ const reservacionActual : Reservacion = {
 
 function validarHuesped (nombreHuesped: string): void{
     if (!nombreHuesped) {
-        console.log("Debe ingresar un nombre válido"); 
+        console.log("Debe ingresar un nombre válido");
+        console.log("----------------------"); 
         
     }
     switch (nombreHuesped) {
         case "Maria":
             console.log("El cliente es incorrecto");
+            console.log("----------------------");
             break;
         
         case "Sergio":  
             console.log("El cliente es correcto");
+            console.log("----------------------");
             break;
 
         default:
             console.log("Cliente no encontrado");
+            console.log("----------------------");
 }
 }
 
@@ -39,17 +43,40 @@ function validarCantidadNoches(reservacionActual: Reservacion): void {
     switch (true) {
         case (reservacionActual.cantidadNoches >= 3):
     console.log("La Reserva se efectua con éxito");
+    console.log("----------------------");
     break;
 
-  case (reservacionActual.cantidadNoches < 3):
+  case (reservacionActual.cantidadNoches > 1 && reservacionActual.cantidadNoches < 3):
     console.log("La cantidad minima de noches son 3");
+    console.log("----------------------");
     break;
 
   default:
     console.log("Debe colocar una cantidad de noches válida");
+    console.log("----------------------");
         
     }
 }
+
+function validarPagoRealizado(reservacionActual: Reservacion): void {
+    switch (reservacionActual.pagoRealizado) {
+        case true:
+            console.log("El pago se ha realizado correctamente");
+            console.log("----------------------");
+            break;
+
+        case false:
+            console.log("El pago no se ha realizado");
+            console.log("----------------------");
+            break;
+
+        default:
+            console.log("Debe colocar un estado de pago válido");
+            console.log("----------------------");
+    }
+}
+
+// ----------- Llamamadas a las funciones de validación -----------
 
 
 validarHuesped("Sergio");
@@ -66,9 +93,12 @@ validarCantidadNoches(reserva1);
 // Ejemplo 2: menor a 3
 const reserva2: Reservacion = {
   nombreHuesped: "Lucía",
-  cantidadNoches: 2,
+  cantidadNoches: 0,
   pagoRealizado: false
 };
 
 validarCantidadNoches(reserva2); 
+
+validarPagoRealizado(reserva1);
+validarPagoRealizado(reserva2);
 
